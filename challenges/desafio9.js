acd4b754ec5618747e32bd5af1586692107cdb8a
Diego Silva <coxcorp@gmail.com>
2022-05-16 15:57:32 -0300

    desafio09

---
+++ challenges/desafio9.js
@@ -0,0 +1,14 @@
+db.produtos.find({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "calorias",
+      quantidade: {
+        $lt: 500,
+      },
+    },
+  },
+},
+  {
+    nome: true,
+    _id: false,
+  });
